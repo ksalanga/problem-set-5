d6500e307fcf3f3336d6d8ec85c3e262ddb4352a
Kenneth Salanga <ksalanga@ucvts.tec.nj.us>
2018-11-20 14:39:03 -0500

    first commit

--- src/Tester.java
+++ src/Tester.java
@@ -17,5 +17,16 @@ public class Tester {
 		 * from our very primitive database (i.e., a flat-file). After making changes, of course,
 		 * you'll need to update the database accordingly.
 		 */
+		
+		/** try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+			String line;
+			
+			while((line = br.readLine()) != null) {
+				//process line
+				//build account from parsed data
+			}
+		}
+		**/
+		
 	}
 }
--- src/User.java
+++ src/User.java
@@ -11,4 +11,135 @@
 
 public class User {
 	
+	private int pin;
+	private String firstName;
+	private String lastName;
+	private String dob;
+	private int phone;
+	private String streetAddress;
+	private String city;
+	private String state;
+	private String postalCode;
+	
+	/**
+	 * Constructor for User class.
+	 * 
+	 * @param name
+	 * @param dob
+	 * @param address
+	 */
+	
+	public User(int pin, String firstName, String lastName, String dob, int phone, String streetAddress, String city, String state, String postalCode) {
+		this.pin = pin;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = dob;
+		this.streetAddress = streetAddress;
+		this.city = city;
+		this.state = state;
+		this.postalCode = postalCode;
+	}
+	
+	/////////////////////////////////// GETTERS AND SETTERS ///////////////////////////////////
+	
+	/**
+	 * Retrieves the user's PIN.
+	 * 
+	 * @return pin
+	 */
+	
+	public int getPIN() {
+		return pin;
+	}
+	
+	/**
+	 * Retrieves the user's name.
+	 * 
+	 * @return name
+	 */
+	
+	public String getfirstName() {
+		return firstName;
+	}
+	
+	public String getlastName() {
+		return lastName;
+	}
+	
+	/**
+	 * Retrieves the user's date of birth.
+	 * 
+	 * @return dob
+	 */
+	
+	public String getDOB() {
+		return dob;
+	}
+	
+	public int getPhone() {
+		return phone;
+	}
+	/**
+	 * Retrieves the user's address.
+	 * 
+	 * @return address
+	 */
+	
+	public String getstreetAddress() {
+		return streetAddress;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public String getState() {
+		return state;
+	}
+	
+	public String getpostalCode() {
+		return postalCode;
+	}
+	
+	/**
+	 * Sets the value of the user's PIN.
+	 * 
+	 * @param pin the new PIN
+	 */
+	
+	public void setPIN(int pin) {
+		this.pin = pin;
+	}
+	
+	/**
+	 * Sets the value of the user's name.
+	 * 
+	 * @param name the new name
+	 */
+	
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	
+	/**
+	 * Sets the value of the user's address.
+	 * 
+	 * @param address the new address
+	 */
+	
+	public void setstreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public void setpostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
 }
